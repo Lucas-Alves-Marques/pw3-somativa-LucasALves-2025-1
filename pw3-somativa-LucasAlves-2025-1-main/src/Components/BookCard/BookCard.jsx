9e873e033543f013c6240e30cfcd3ebc310aabb1
Lucas Alves Marques <160551088+Lucas-Alves-Marques@users.noreply.github.com>
2025-04-24 22:10:02 -0300

    Criando o List do Projeto

---
+++ pw3-somativa-LucasAlves-2025-1-main/src/Components/BookCard/BookCard.jsx
@@ -0,0 +1,18 @@
+import Button from '../Button/Button'
+
+import style from './BookCard.module.css'
+
+const BookCard = ({cod_livro, nome_livro, autor_livro, img_livro})=>{
+    return(
+        
+        <div className={style.bookCard}>
+            <h3 className={style.titulo}>{nome_livro}</h3>
+            <p className={style.autor}>{autor_livro}</p>
+            <img src={img_livro} alt="Capa: As Cavernas de Aço" />
+            <Button label='DETALHE' />
+        </div>
+        
+    )
+}
+
+export default BookCard
